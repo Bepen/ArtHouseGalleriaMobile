9ad1e54351f8a333b7281e6352f2707f70586379
Bepen Neupane <bepen3@gmail.com>
2019-04-13 15:04:52 -0400

    title and date not showing up part 2

--- app/src/main/java/com/example/arthouseapp/MobileArrayAdapter.java
+++ app/src/main/java/com/example/arthouseapp/MobileArrayAdapter.java
@@ -38,7 +38,7 @@ public class MobileArrayAdapter extends ArrayAdapter<String> {
 
         Item0.titleArr = new String[] {"Image One", "Image Two"};
         Item0.authorArr = new String[] {"Person One", "Person Two"};
-        Item0.dateArr = new String[] {"1/1/1", "2/2/2"};
+        Item0.dateArr = new String[] {"1-1-1", "2-2-2"};
         Item0.descArr = new String[] {"Description One", "Description Two"};
 
         int id = context.getResources().getIdentifier(s, "drawable", context.getPackageName());
