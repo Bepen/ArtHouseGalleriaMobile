954090e376b5835fb7efd74493c6914edaf8863d
Bepen Neupane <bepen3@gmail.com>
2019-04-19 16:24:02 -0400

    Added login screen

---
+++ app/src/main/java/com/example/arthouseapp/Login.java
@@ -0,0 +1,71 @@
+package com.example.arthouseapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    String[] users = {"User1", "User2"};
+    String[] passwords = {"Pass1", "Pass2"};
+    int userIndex = 0;
+    static TextView tryAgain;
+    EditText uText;
+    EditText pText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if(ProfileFragment.loggedIn){
+            finish();
+        }
+
+
+        uText = (EditText) findViewById(R.id.userText);
+        pText = (EditText) findViewById(R.id.passText);
+
+
+        tryAgain = (TextView) findViewById(R.id.tryAgainText);
+
+
+        Button button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(ProfileFragment.loggedIn){
+                    finish();
+                }
+                else{
+                    if(tryLogin(uText.getText().toString(), pText.getText().toString())){
+                        ProfileFragment.loggedIn = true;
+                        finish();
+                    }else {
+                        tryAgain.setText("Try Again");
+                    }
+                }
+            }
+        });
+    }
+
+    protected boolean tryLogin(String uInput, String pInput){
+        for(int i = 0; i < users.length; i++){
+            if(users[i].equals(uInput)){
+                userIndex = i;
+                if(pInput.equals(passwords[userIndex])){
+                    return true;
+                }
+            }
+
+        }
+
+
+        return false;
+    }
+}
--- app/src/main/java/com/example/arthouseapp/ProfileFragment.java
+++ app/src/main/java/com/example/arthouseapp/ProfileFragment.java
@@ -1,4 +1,5 @@
 package com.example.arthouseapp;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,10 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
+
+    static boolean loggedIn = false;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_profile, null);
+        View view = inflater.inflate(R.layout.fragment_profile, null);
+
+        if(!loggedIn){
+            Intent myIntent = new Intent(view.getContext(), Login.class);
+            startActivity(myIntent);
+        }
+
+        return view;
     }
 }
